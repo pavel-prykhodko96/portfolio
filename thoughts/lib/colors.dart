810379d0bcda9f13f44d2fbe74a937f1c89887a9
solid-pavloprykhodko <pavlo.prykhodko@solid.software>
2021-02-24 23:00:52 +0200

    Added UI for the home view with tabs, left menu, configured Firebase and connected to that

---
+++ thoughts/lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color whiteColor = Colors.white;
+const Color blackColor = Colors.black;
+const Color actionColor = Color.fromARGB(255, 105, 109, 198);
+const Color themeColor = Color.fromARGB(255, 159, 177, 217);
---
+++ thoughts/lib/widgets/thoughtsLabel.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ThoughtsLabel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+      child: Image.asset("assets/thoughtsLabel.png"),
+    );
+  }
+}
